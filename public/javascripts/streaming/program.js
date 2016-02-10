0e400645998f1573d1cedad63d1ddcfade94962e
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2016-02-09 21:56:16 -0500

    adds some stuff to gitignore, can now see ugly info about the currently playing song in plaer window

--- public/javascripts/streaming/program.js
+++ public/javascripts/streaming/program.js
@@ -1,6 +1,16 @@
 /**
  * Created by davidmesa on 2/6/16.
  */
-$(document).ready(function(){
-
+$(document).ready(function () {
+    window.setInterval(function () {
+        $.getJSON("/api/airtimeInfo", function(airtimeInfo) {
+            showSongInfo(airtimeInfo);
+        });
+    }, 25000); // ask every 25 seconds
 });
+
+function showSongInfo(songInfo) {
+    $("#song-name").html(songInfo.song.name);
+    $("#song-artist").html(songInfo.song.author);
+    $("#song-picture").attr("src", songInfo.song.url);
+}
